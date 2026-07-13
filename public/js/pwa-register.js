03b6af1bd7b6965e4439797323c59dfb069469ff
fadliachsan27 <fadliachsan010@gmail.com>
2026-07-13 16:20:03 +0700

    Update pwa-register.js

--- public/js/pwa-register.js
+++ public/js/pwa-register.js
@@ -4,7 +4,29 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
       .register('/sw.js')
-      .then((reg) => console.log('[PWA] Service worker terdaftar:', reg.scope))
+      .then((reg) => {
+        console.log('[PWA] Service worker terdaftar:', reg.scope);
+        reg.update().catch(() => {});
+        if (reg.waiting) {
+          reg.waiting.postMessage({ type: 'SKIP_WAITING' });
+        }
+        reg.addEventListener('updatefound', () => {
+          const newWorker = reg.installing;
+          if (!newWorker) return;
+          newWorker.addEventListener('statechange', () => {
+            if (newWorker.state === 'installed' && navigator.serviceWorker.controller) {
+              newWorker.postMessage({ type: 'SKIP_WAITING' });
+            }
+          });
+        });
+      })
       .catch((err) => console.warn('[PWA] Gagal mendaftarkan service worker:', err));
+
+    let reloaded = false;
+    navigator.serviceWorker.addEventListener('controllerchange', () => {
+      if (reloaded) return;
+      reloaded = true;
+      window.location.reload();
+    });
   });
 }
